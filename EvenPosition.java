eec76798de24ff9edba9cc566e1a30c6e8f847e1
unknown <sanjana@example.com>
2022-04-07 22:23:21 +0530

    To print the elements of an array present on an even position

---
+++ EvenPosition.java
@@ -0,0 +1,12 @@
+package com.Bridgelabz.Day03PracticeProblems;
+
+public class EvenPosition {
+    public static void main(String[] args) {
+        int arr[] = new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        System.out.println("Elements of given array:");
+        for (int i = 1; i < arr.length; i = i+2) {
+            System.out.println("Even position of elements: " +arr[i]);
+        }
+    }
+}
+
