10f63c89153fa0a80655ee7c764954ab07948419
unknown <sanjana@example.com>
2022-04-07 22:16:03 +0530

    Java Program to print the elements of an array

---
+++ PrintArray.java
@@ -0,0 +1,15 @@
+//Java Program to print the elements of an array
+package com.Bridgelabz.Day03PracticeProblems;
+
+public class PrintArray {
+    public static void main(String[] args) {
+        //initialize array
+        int arr[] = {10,20,30,40,50};
+        //loop through the array by incrementing value of i
+        for (int i = 0; i < arr.length; i++) {
+           System.out.println(arr[i]);
+           //System.out.print(arr[i]+ ",");
+            }
+        }
+    }
+
