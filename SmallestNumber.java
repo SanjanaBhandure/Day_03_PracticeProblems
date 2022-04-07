6616fdfcc7a1c3231faa3f3d673a9ea67ed62d05
unknown <sanjana@example.com>
2022-04-07 22:21:33 +0530

    To print the smallest number in an array

---
+++ SmallestNumber.java
@@ -0,0 +1,28 @@
+//ava Program to print the smallest element in an array
+package com.Bridgelabz.Day03PracticeProblems;
+import java.util.Scanner;
+
+public class SmallestNumber {
+    public static void main(String[] args) {
+        int number, min;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of elements in the array:");
+        number = sc.nextInt();
+        int arr[] = new int[number];
+        System.out.println("Enter the elements of array:");
+        for(int i = 0; i < number; i++)
+        {
+            arr[i] = sc.nextInt();
+        }
+        min = arr[0];
+        for(int i = 0; i < number; i++)
+        {
+            if(min > arr[i])
+            {
+                min = arr[i];
+            }
+        }
+        System.out.println("Minimum value in the array is: " +min);
+    }
+    }
+
