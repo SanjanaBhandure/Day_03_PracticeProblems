9afc49ccf8e166ab361b26376e81db35071db9ab
unknown <sanjana@example.com>
2022-04-07 22:29:16 +0530

    To print the elements of an array present on odd position

---
+++ OddPosition.java
@@ -0,0 +1,11 @@
+package com.Bridgelabz.Day03PracticeProblems;
+
+public class OddPosition {
+    public static void main(String[] args) {
+        int arr[] = new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        System.out.println("Elements of given array:");
+        for (int i = 0; i < arr.length; i = i+2) {
+            System.out.println("Odd position of elements: " +arr[i]);
+        }
+    }
+}
