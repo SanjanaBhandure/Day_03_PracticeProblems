7e3c69a192d6e46c7b5c2e2a1fb255cd0dbf9fa0
unknown <sanjana@example.com>
2022-04-07 22:19:47 +0530

    To print largest element of an array

---
+++ LargestNumber.java
@@ -0,0 +1,29 @@
+//Java Program to print the largest element in an array
+package com.Bridgelabz.Day03PracticeProblems;
+import java.util.Scanner;
+
+public class LargestNumber {
+    public static void main(String[] args)
+    {
+        int number, max;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of elements in the array: ");
+        number = sc.nextInt();
+        int arr[] = new int[number];
+        System.out.println("Enter the elements of array: ");
+        for(int i = 0; i < number; i++)
+        {
+            arr[i] = sc.nextInt();
+        }
+        max = arr[0];
+        for(int i = 0; i < number; i++)
+        {
+            if(max < arr[i])
+            {
+                max = arr[i];
+            }
+        }
+        System.out.println("Maximum value in the array is: "+max);
+    }
+}
+
